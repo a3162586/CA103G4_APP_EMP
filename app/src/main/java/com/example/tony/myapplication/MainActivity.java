5f790890280007fe3ae16ebb24168bf8dfa7a0a6
a3162586 <42692256+a3162586@users.noreply.github.com>
2018-09-10 00:46:25 +0800

    add three fragment for order serve delivery

--- app/src/main/java/com/example/tony/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tony/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -22,26 +25,32 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private GridView gdTable;
-    private List<MyTable.Table> tableList;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            //切換fragment畫面(點餐、出餐、外送)
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_order:
-                    mTextMessage.setText(R.string.title_order);
+                    fragmentTransaction.replace(R.id.frameLayout,new OrderFragment());
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_serve:
-                    mTextMessage.setText(R.string.title_serve);
+                    fragmentTransaction.replace(R.id.frameLayout,new ServeFragment());
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_delivery:
-                    mTextMessage.setText(R.string.title_delivery);
+                    fragmentTransaction.replace(R.id.frameLayout,new DeliveryFragment());
+                    fragmentTransaction.commit();
                     return true;
             }
             return false;
@@ -52,14 +61,11 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initTables();
-        findView();
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-
+        //底部Menu的ICON大小調整
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) navigation.getChildAt(0);
         for (int i = 0; i < menuView.getChildCount(); i++) {
             final View iconView = menuView.getChildAt(i).findViewById(android.support.design.R.id.icon);
@@ -69,83 +75,18 @@ public class MainActivity extends FragmentActivity {
             layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 32, displayMetrics);
             iconView.setLayoutParams(layoutParams);
         }
+
+        initFragment();
     }
 
-    private void initTables() {
-        tableList = new ArrayList<>();
-        for(int i=1 ; i<=MyTable.TOTALTABLES ; i++)
-            tableList.add(new MyTable.Table(i,R.drawable.table,"空桌"));
-    }
-
-    private void findView() {
-        gdTable = findViewById(R.id.gvTable);
-        gdTable.setAdapter(new TableAdpter(this,tableList));
-        gdTable.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                MyTable.Table table = (MyTable.Table) adapterView.getItemAtPosition(i);
-                ImageView imageView = new ImageView(MainActivity.this);
-                imageView.setImageResource(table.getTableImg());
-                Toast toast = new Toast(MainActivity.this);
-                toast.setView(imageView);
-                toast.setDuration(Toast.LENGTH_SHORT);
-                toast.show();
-
-            }
-        });
-    }
-
-    public class TableAdpter extends BaseAdapter {
-
-        private LayoutInflater layoutInflater;
-        private List<MyTable.Table> tableList;
-
-        public TableAdpter(Context context,List<MyTable.Table> tableList) {
-            this.tableList = tableList;
-            layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public int getCount() {
-            return tableList.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return tableList.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return tableList.get(i).getTableNo();
-        }
-
-        @Override
-        public View getView(int i, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            if (convertView == null) {
-                convertView = layoutInflater.inflate(R.layout.gridview_table, parent, false);
-                holder = new ViewHolder();
-                holder.ivTableImg = convertView.findViewById(R.id.ivTableImg);
-                holder.tvTableStatus = convertView.findViewById(R.id.tvTableStatus);
-                holder.tvTableNo = convertView.findViewById(R.id.tvTableNo);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-            MyTable.Table table = tableList.get(i);
-            holder.ivTableImg.setImageResource(table.getTableImg());
-            holder.tvTableStatus.setText(table.getTableStatus());
-            holder.tvTableNo.setText(Integer.toString(table.getTableNo()));
-
-            return convertView;
-        }
-
-        private class ViewHolder {
-            ImageView ivTableImg;
-            TextView tvTableNo, tvTableStatus;
-        }
+    //依權限登入後預設畫面
+    private void initFragment() {
+        OrderFragment orderFragment = new OrderFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.frameLayout,orderFragment)
+                .show(orderFragment)
+                .commit();
     }
 
 }
